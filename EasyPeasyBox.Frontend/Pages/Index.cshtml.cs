651d418488d6f7739ff752328e4256db3f804511
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2023-08-04 16:08:15 +0200

    init commit

---
+++ EasyPeasyBox.Frontend/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EasyPeasyBox.Frontend.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            var result = await client.GetAsync("http://easypeasybox.backend/WeatherForecast");
+            if (result.IsSuccessStatusCode)
+            {
+                var text = await result.Content.ReadAsStringAsync();
+                ViewData["WeatherForecast"] = text;
+            }
+            else
+            {
+
+            }
+        }
+        return Page();
+    }
+}
